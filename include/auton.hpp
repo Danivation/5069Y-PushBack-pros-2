0db3f81ea95f3714eaf464fe21572cd9bed6db60
Daniel Velasco <divelasco7@gmail.com>
2025-09-08 13:45:21 -0500

    impl: new auton filler functions

--- include/auton.hpp
+++ include/auton.hpp
@@ -5,6 +5,16 @@
 void auton_none();
 void auton_red_right_1();
 void auton_red_right_2();
+void auton_red_right_3();
+void auton_red_left_1();
+void auton_red_left_2();
+void auton_red_left_3();
+void auton_blue_right_1();
+void auton_blue_right_2();
+void auton_blue_right_3();
+void auton_blue_left_1();
+void auton_blue_left_2();
+void auton_blue_left_3();
 void auton_skills();
 
 // Auton selector
--- src/auton.cpp
+++ src/auton.cpp
@@ -4,25 +4,25 @@ const std::vector<auton_descriptor_t> red_right_autons = {
     {"None", "0 points", auton_none},
     {"Red Right 1", "1 point", auton_red_right_1},
     {"Auton 2", "5 points", auton_red_right_2},
-    {"Auton 3", "12 points"}
+    {"Auton 3", "12 points", auton_red_right_3}
 };
 const std::vector<auton_descriptor_t> red_left_autons = {
-    {"None", "0 points"},
-    {"Red Left 1", "1 point"},
-    {"Auton 2", "5 points"},
-    {"Auton 3", "12 points"}
+    {"None", "0 points", auton_none},
+    {"Red Left 1", "1 point", auton_red_left_1},
+    {"Auton 2", "5 points", auton_red_left_2},
+    {"Auton 3", "12 points", auton_red_left_3}
 };
 const std::vector<auton_descriptor_t> blue_right_autons = {
-    {"None", "0 points"},
-    {"Blue Right 1", "1 point"},
-    {"Auton 2", "5 points"},
-    {"Auton 3", "12 points"}
+    {"None", "0 points", auton_none},
+    {"Blue Right 1", "1 point", auton_blue_right_1},
+    {"Auton 2", "5 points", auton_blue_right_2},
+    {"Auton 3", "12 points", auton_blue_right_3}
 };
 const std::vector<auton_descriptor_t> blue_left_autons = {
-    {"None", "0 points"},
-    {"Blue Left 1", "1 point"},
-    {"Auton 2", "5 points"},
-    {"Auton 3", "12 points"}
+    {"None", "0 points", auton_none},
+    {"Blue Left 1", "1 point", auton_blue_left_1},
+    {"Auton 2", "5 points", auton_blue_left_2},
+    {"Auton 3", "12 points", auton_blue_left_3}
 };
 const auton_descriptor_t skills_auton = {"Skills", "0 points", auton_skills};
 
@@ -67,6 +67,46 @@ void auton_red_right_1() {
 
 void auton_red_right_2() {
     pros::lcd::print(0, "hi from red right 2");
+}
+
+void auton_red_right_3() {
+    pros::lcd::print(0, "hi from red right 3");
+}
+
+void auton_red_left_1() {
+    pros::lcd::print(0, "hi from red left 1");
+}
+
+void auton_red_left_2() {
+    pros::lcd::print(0, "hi from red left 2");
+}
+
+void auton_red_left_3() {
+    pros::lcd::print(0, "hi from red left 3");
+}
+
+void auton_blue_right_1() {
+    pros::lcd::print(0, "hi from blue right 1");
+}
+
+void auton_blue_right_2() {
+    pros::lcd::print(0, "hi from blue right 2");
+}
+
+void auton_blue_right_3() {
+    pros::lcd::print(0, "hi from blue right 3");
+}
+
+void auton_blue_left_1() {
+    pros::lcd::print(0, "hi from blue left 1");
+}
+
+void auton_blue_left_2() {
+    pros::lcd::print(0, "hi from blue left 2");
+}
+
+void auton_blue_left_3() {
+    pros::lcd::print(0, "hi from blue left 3");
 }
 
 void auton_skills() {
