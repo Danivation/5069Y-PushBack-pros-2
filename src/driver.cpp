dde68f7e32ce05e7465ac743cfbbf44fa5a0d2c8
Daniel Velasco <divelasco7@gmail.com>
2025-09-11 19:35:35 -0500

    feat: formatting

--- src/driver.cpp
+++ src/driver.cpp
@@ -4,11 +4,11 @@
 #define TURN_AXIS               master.get_analog(ANALOG_RIGHT_X)
 #define INTAKE_TO_STORAGE       master.get_digital(DIGITAL_R1)
 #define INTAKE_TO_LOW_GOAL      master.get_digital(DIGITAL_R2)
-#define INTAKE_TO_HIGH_GOAL     master.get_digital(DIGITAL_L1)
-#define INTAKE_TO_MID_GOAL      master.get_digital(DIGITAL_L2)
+#define INTAKE_TO_MID_GOAL      master.get_digital(DIGITAL_L1)
+#define INTAKE_TO_HIGH_GOAL     master.get_digital(DIGITAL_L2)
 #define STORAGE_DRAIN_TOGGLE    master.get_digital(DIGITAL_B)
-#define STORAGE_REVERSE_TOGGLE  master.get_digital(DIGITAL_DOWN)
-#define LOADER_TOGGLE           master.get_digital(DIGITAL_A)
+#define STORAGE_REVERSE_TOGGLE  master.get_digital(DIGITAL_A)
+#define LOADER_TOGGLE           master.get_digital(DIGITAL_DOWN)
 
 void DrivetrainControl() {
     float throttle;
@@ -27,30 +27,30 @@ void IntakeControl() {
     while (true) {
         if (INTAKE_TO_STORAGE) {
             // storage: spin intake forwards, spin front forwards, do not drain, set hood to storage
-            intake_bottom.move_voltage(12000);
-            if (!ColorStop) intake_front.move_voltage(12000);
+            intake_bottom.move(127);
+            if (!ColorStop) intake_front.move(127);
             intake_back.brake();
             if (!hood_piston.is_extended()) hood_piston.extend();
         } else if (INTAKE_TO_LOW_GOAL) {
             // low goal: spin intake reverse, spin front reverse, drain if draining, set hood to storage
-            intake_bottom.move_voltage(-12000);
-            intake_front.move_voltage(-12000);
-            if (StorageDrain) intake_back.move_voltage(12000);
+            intake_bottom.move(-127);
+            intake_front.move(-127);
+            if (StorageDrain) intake_back.move(127);
             else intake_back.brake();
-            if (!hood_piston.is_extended()) hood_piston.extend();
+            //if (!hood_piston.is_extended()) hood_piston.extend();
         } else if (INTAKE_TO_MID_GOAL) {
             // mid goal: spin intake forwards, spin front reverse, drain if draining, set hood to storage
             // figure out some way to get it to go to high goal if it gets color sorted here
-            intake_bottom.move_voltage(12000);
-            if (!ColorStop) intake_front.move_voltage(-12000);
-            if (StorageDrain) intake_back.move_voltage(12000);
+            intake_bottom.move(127);
+            if (!ColorStop) intake_front.move(-127);
+            if (StorageDrain) intake_back.move(127);
             else intake_back.brake();
-            if (!hood_piston.is_extended()) hood_piston.extend();
+            //if (!hood_piston.is_extended()) hood_piston.extend();
         } else if (INTAKE_TO_HIGH_GOAL) {
             // high goal: spin intake forwards, spin front forwards, drain if draining, set hood to high goal
-            intake_bottom.move_voltage(12000);
-            if (!ColorStop) intake_front.move_voltage(12000);
-            if (StorageDrain) intake_back.move_voltage(12000);
+            intake_bottom.move(127);
+            if (!ColorStop) intake_front.move(127);
+            if (StorageDrain) intake_back.move(127);
             else intake_back.brake();
             if (hood_piston.is_extended()) hood_piston.retract();
         } else {
